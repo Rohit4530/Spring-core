ba1058eba5302e2df38cc6a5ed380f5a590f4cdc
789273 <rohitgunjal547@gmail.com>
2022-08-20 14:34:43 +0530

    added

---
+++ SpringCore/src/main/java/com/spring/core/SpringCore/App.java
@@ -0,0 +1,14 @@
+package com.spring.core.SpringCore;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App 
+{
+    public static void main( String[] args ){
+      ApplicationContext context=new ClassPathXmlApplicationContext("Config.xml");
+      Student student= (Student) context.getBean("student1");
+      System.out.println(student);
+    }
+}
+ 
